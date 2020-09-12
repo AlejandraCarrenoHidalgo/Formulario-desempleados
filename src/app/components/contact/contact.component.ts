7d99f1a85042428822cff3b69749a8cb2f11cdb1
Alejandra <alejandra.carrenohidalgo@gmail.com>
2020-09-12 07:35:29 -0500

    formulario Angular

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { DataDbService } from '../../services/data-db.service';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'contactForm',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
+})
+export class ContactComponent implements OnInit {
+  createFormGroup() {
+    return new FormGroup({
+      email: new FormControl(' '),
+      name: new FormControl(' '),
+      message: new FormControl(' '),
+    });
+  }
+
+  contactForm: FormGroup;
+  constructor(private dbData: DataDbService) {
+    this.contactForm = this.createFormGroup();
+  }
+
+  ngOnInit() {}
+
+  onResetForm() {
+    this.contactForm.reset();
+  }
+  onSaveForm() {
+    console.log('saved');
+    const newContact = {
+      name: 'Aleja',
+      email: 'zafiri@gmail.com',
+      message: 'Hola',
+    };
+    this.dbData.saveMessage(newContact);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBXRx1MA4S0Gu2yQ8RuFUih9aBBDkpYuv8',
+    authDomain: 'formcontact-604d0.firebaseapp.com',
+    databaseURL: 'https://formcontact-604d0.firebaseio.com',
+    projectId: 'formcontact-604d0',
+    storageBucket: 'formcontact-604d0.appspot.com',
+    messagingSenderId: '836010033377',
+    appId: '1:836010033377:web:06e1cb76cf40d0ae8277a0',
+    measurementId: 'G-NYQCVGXZ7K',
+  },
 };
 
 /*
